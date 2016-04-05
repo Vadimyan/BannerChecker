9d76fa5ee9490ec9c96af0b7a87d622d925ed27a
va_martynov <va_martynov@skbkontur.ru>
2016-04-06 00:02:18 +0300

    Wpf command infrastructure

---
+++ BannerChecker.Wpf/Common/CommandBase.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Input;
+
+namespace BannerChecker.Wpf.Common
+{
+	public abstract class CommandBase : INotifyPropertyChanged, ICommand
+	{
+		private class Listener<TEventArgs> : IWeakEventListener where TEventArgs : EventArgs
+		{
+			private readonly EventHandler<TEventArgs> realHandler;
+
+			public Listener(EventHandler<TEventArgs> handler)
+			{
+				realHandler = handler;
+			}
+
+			bool IWeakEventListener.ReceiveWeakEvent(Type managerType, Object sender, EventArgs e)
+			{
+				ExecuteRealHandler(sender, e);
+				return true;
+			}
+
+			private void ExecuteRealHandler(object sender, EventArgs e)
+			{
+				var realArgs = (TEventArgs) e;
+				realHandler(sender, realArgs);
+			}
+		}
+
+		private readonly Listener<PropertyChangedEventArgs> weakEventListener;
+
+		public event EventHandler CanExecuteChanged;
+
+		protected CommandBase()
+		{
+			weakEventListener = new Listener<PropertyChangedEventArgs>(RequeryCanExecute);
+		}
+
+		public abstract bool CanExecute(object parameter);
+		protected abstract void PerformExecute(object parameter);
+
+		public void Execute(object parameter)
+		{
+			BeforeExecure();
+			PerformExecute(parameter);
+			AfrerExecute();
+		}
+
+		public virtual void InvalidateCommand()
+		{
+			RaiseCanExecuteChanged();
+		}
+
+		protected void AddListenerInternal<TEntity>(TEntity source, string propertyName)
+			where TEntity : INotifyPropertyChanged
+		{
+			PropertyChangedEventManager.AddListener(source, weakEventListener, propertyName);
+		}
+
+		protected virtual void BeforeExecure()
+		{
+		}
+
+		protected virtual void AfrerExecute()
+		{
+		}
+
+		private void RaiseCanExecuteChanged()
+		{
+			var handler = CanExecuteChanged;
+			handler?.Invoke(this, EventArgs.Empty);
+		}
+
+		private void RequeryCanExecute(object sender, PropertyChangedEventArgs args)
+		{
+			InvalidateCommand();
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+		{
+			var handler = PropertyChanged;
+			handler?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+}
---
+++ BannerChecker.Wpf/Common/RelayCommand.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows.Input;
+
+namespace BannerChecker.Wpf.Common
+{
+	public class RelayCommand : CommandBase
+	{
+		private readonly IEnumerable<Func<bool>> canExecuteCollection;
+		private readonly Action execute;
+
+		public RelayCommand(Action execute)
+			: this(execute, () => true)
+		{
+		}
+
+		public RelayCommand(Action execute, Func<bool> canExecutePredicate)
+			: this(execute, new[] { canExecutePredicate }, new Tuple<INotifyPropertyChanged, string>[0])
+		{
+		}
+
+		public RelayCommand(Action execute, IEnumerable<Func<bool>> canExecutePredicates, IEnumerable<Tuple<INotifyPropertyChanged, string>> invalidators)
+		{
+			this.execute = execute;
+			canExecuteCollection = canExecutePredicates;
+			AddInvalidators(invalidators);
+		}
+
+		private void AddInvalidators(IEnumerable<Tuple<INotifyPropertyChanged, string>> invalidators)
+		{
+			foreach (var invalidator in invalidators)
+				AddListenerInternal(invalidator.Item1, invalidator.Item2);
+		}
+
+		public sealed override bool CanExecute(object parameter)
+		{
+			return canExecuteCollection.All(ce => ce());
+		}
+
+		protected sealed override void PerformExecute(object parameter)
+		{
+			execute();
+		}
+	}
+
+	public class RelayCommand<T> : CommandBase
+	{
+		private readonly IEnumerable<Func<T, bool>> canExecuteCollection;
+		private readonly Action<T> execute;
+
+		public RelayCommand(Action<T> execute)
+			: this(execute, par => true)
+		{
+		}
+
+		public RelayCommand(Action<T> execute, Func<T, bool> canExecutePredicate)
+			: this(execute, new[] { canExecutePredicate }, new Tuple<INotifyPropertyChanged, string>[0], false)
+		{
+		}
+
+		public RelayCommand(Action<T> execute, IEnumerable<Func<T, bool>> canExecutePredicates, IEnumerable<Tuple<INotifyPropertyChanged, string>> invalidators, bool listenCommandManager)
+		{
+			this.execute = execute;
+			canExecuteCollection = canExecutePredicates;
+			AddInvalidators(invalidators);
+			InitializeCommandManager(listenCommandManager);
+		}
+		
+		public sealed override bool CanExecute(object parameter)
+		{
+			var par = (T)parameter;
+			return canExecuteCollection.All(ce => ce(par));
+		}
+
+		protected sealed override void PerformExecute(object parameter)
+		{
+			execute((T)parameter);
+		}
+
+		private void InitializeCommandManager(bool listenCommandManager)
+		{
+			if (listenCommandManager)
+				CommandManager.RequerySuggested += (sender, args) => InvalidateCommand();
+		}
+
+		private void AddInvalidators(IEnumerable<Tuple<INotifyPropertyChanged, string>> invalidators)
+		{
+			foreach (var invalidator in invalidators)
+				AddListenerInternal(invalidator.Item1, invalidator.Item2);
+		}
+	}
+}
