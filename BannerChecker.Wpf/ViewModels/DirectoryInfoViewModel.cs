5777d89421a21648ad2c6e2597ff94a65b4a24d3
va_martynov <va_martynov@skbkontur.ru>
2016-04-06 00:48:03 +0300

    DirectoryInfoViewModel mock

---
+++ BannerChecker.Wpf/ViewModels/DirectoryInfoViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.ObjectModel;
+using BannerChecker.Lib.FileInfo;
+using BannerChecker.Wpf.Common;
+
+namespace BannerChecker.Wpf.ViewModels
+{
+	public class DirectoryInfoViewModel : ViewModelBase
+	{
+		private string _directoryPath;
+		public string DirectoryPath
+		{
+			get { return _directoryPath; }
+			set
+			{
+				_directoryPath = value;
+				OnPropertyChanged();
+				UpdateDirectoryFilesInfo(value);
+			}
+		}
+		
+		public ObservableCollection<ImageInfo> DirectoryFilesInfo { get; set; } = new ObservableCollection<ImageInfo>();
+
+		public DirectoryInfoViewModel()
+		{
+			
+		}
+
+		private void UpdateDirectoryFilesInfo(string value)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
--- BannerChecker.Wpf/Views/DirectoryInfoView.xaml.cs
+++ BannerChecker.Wpf/Views/DirectoryInfoView.xaml.cs
@@ -1,24 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
 
 namespace BannerChecker.Wpf.Views
 {
 	/// <summary>
 	/// Interaction logic for DirectoryInfoView.xaml
 	/// </summary>
-	public partial class DirectoryInfoView : UserControl
+	public partial class DirectoryInfoView
 	{
 		public DirectoryInfoView()
 		{
